603e2fca8dc54cae4cf9a4dabfc54d11c4cca035
achilj <39186535+achilj@users.noreply.github.com>
2020-08-06 18:35:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.login(process.env.token);
 client.on("ready", async () =>{
 
     console.log(`${client.user.username} Is Online!`);
-    client.user.setActivity("controleerd jullie maagdheid!", {type: "LOOKING"})
+    client.user.setActivity("controleerd jullie maagdheid!", {type: "PLAYING"})
 
 });
 
